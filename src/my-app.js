606b947dee847ae3b1687311294eb8fb5b4d0ed2
levi <lvikng56@gmail.com>
2018-10-02 09:27:04 -0500

    first commit

---
+++ src/my-app.js
@@ -0,0 +1,197 @@
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import { setPassiveTouchGestures, setRootPath } from '@polymer/polymer/lib/utils/settings.js';
+import '@polymer/app-layout/app-drawer/app-drawer.js';
+import '@polymer/app-layout/app-drawer-layout/app-drawer-layout.js';
+import '@polymer/app-layout/app-header/app-header.js';
+import '@polymer/app-layout/app-header-layout/app-header-layout.js';
+import '@polymer/app-layout/app-scroll-effects/app-scroll-effects.js';
+import '@polymer/app-layout/app-toolbar/app-toolbar.js';
+import '@polymer/app-route/app-location.js';
+import '@polymer/app-route/app-route.js';
+import '@polymer/iron-pages/iron-pages.js';
+import '@polymer/iron-selector/iron-selector.js';
+import '@polymer/paper-icon-button/paper-icon-button.js';
+import './my-icons.js';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCFEdd5K0ueU3Lfjb_pzbtSvW1qeJeO0ys",
+    authDomain: "givecherry-app.firebaseapp.com",
+    databaseURL: "https://givecherry-app.firebaseio.com",
+    projectId: "givecherry-app",
+    storageBucket: "givecherry-app.appspot.com",
+    messagingSenderId: "133287100960"
+  };
+firebase.initializeApp(config);
+
+// Gesture events like tap and track generated from touch will not be
+// preventable, allowing for better scrolling performance.
+setPassiveTouchGestures(true);
+
+// Set Polymer's root path to the same value we passed to our service worker
+// in `index.html`.
+setRootPath(MyAppGlobals.rootPath);
+
+class MyApp extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          --app-primary-color: #ee2525;
+          --app-secondary-color: white;
+
+          display: block;
+        }
+
+        app-drawer-layout:not([narrow]) [drawer-toggle] {
+          display: none;
+        }
+
+        app-header {
+          color: #fff;
+          background-color: var(--app-primary-color);
+        }
+
+        app-header paper-icon-button {
+          --paper-icon-button-ink-color: white;
+        }
+
+        .drawer-list {
+          margin: 0 20px;
+        }
+
+        .drawer-list a {
+          display: block;
+          padding: 0 16px;
+          text-decoration: none;
+          color: var(--app-secondary-color);
+          line-height: 40px;
+        }
+
+        .drawer-list a.iron-selected {
+          color: black;
+          font-weight: bold;
+        }
+      </style>
+
+      <firebase-app auth-domain="givecherry-app.firebaseapp.com"
+        database-url="https://givecherry-app.firebaseio.com"
+        api-key="AIzaSyCFEdd5K0ueU3Lfjb_pzbtSvW1qeJeO0ys"></firebase-app>
+      <firebase-auth id="auth" user="{{user}}" provider="google" on-error="showError"></firebase-auth>
+
+      <app-location route="{{route}}" url-space-regex="^[[rootPath]]">
+      </app-location>
+
+      <app-route route="{{route}}" pattern="[[rootPath]]:page" data="{{routeData}}" tail="{{subroute}}">
+      </app-route>
+
+
+      <!-- Main content -->
+        <app-header slot="header" condenses="" reveals="" effects="waterfall">
+          <app-toolbar>
+            <div main-title="">Cherry</div>
+            <paper-icon-button icon="my-icons:account-circle" on-tap="__login"></paper-icon-button>
+            <div>[[user.Q.displayName]]</div>
+          </app-toolbar>
+        </app-header>
+        [[user]]
+        <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
+          <my-view1 name="view1"></my-view1>
+          <my-view2 name="view2"></my-view2>
+          <my-view3 name="view3"></my-view3>
+          <my-view404 name="view404"></my-view404>
+        </iron-pages>
+    `;
+  }
+
+  static get properties() {
+    return {
+      page: {
+        type: String,
+        reflectToAttribute: true,
+        observer: '_pageChanged'
+      },
+      routeData: Object,
+      subroute: Object,
+      user: {
+        type: Object,
+        notify: true
+      }
+    };
+  }
+
+  static get observers() {
+    return [
+      '_routePageChanged(routeData.page)'
+    ];
+  }
+
+  _routePageChanged(page) {
+     // Show the corresponding page according to the route.
+     //
+     // If no page was found in the route data, page will be an empty string.
+     // Show 'view1' in that case. And if the page doesn't exist, show 'view404'.
+    if (!page) {
+      this.page = 'view1';
+    } else if (['view1', 'view2', 'view3'].indexOf(page) !== -1) {
+      this.page = page;
+    } else {
+      this.page = 'view404';
+    }
+
+    // Close a non-persistent drawer when the page & route are changed.
+    // if (!this.$.drawer.persistent) {
+    //   this.$.drawer.close();
+    // }
+  }
+
+  _pageChanged(page) {
+    // Import the page component on demand.
+    //
+    // Note: `polymer build` doesn't like string concatenation in the import
+    // statement, so break it up.
+    switch (page) {
+      case 'view1':
+        import('./my-view1.js');
+        break;
+      case 'view2':
+        import('./my-view2.js');
+        break;
+      case 'view3':
+        import('./my-view3.js');
+        break;
+      case 'view404':
+        import('./my-view404.js');
+        break;
+    }
+  }
+
+  _login(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    var user = firebase.auth().currentUser;
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  }
+
+  __login(){
+    this.$.signInWithPopup()
+  }
+}
+
+window.customElements.define('my-app', MyApp);
---
+++ src/my-icons.js
@@ -0,0 +1,36 @@
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
+import '@polymer/iron-iconset-svg/iron-iconset-svg.js';
+const $_documentContainer = document.createElement('template');
+$_documentContainer.innerHTML = `<iron-iconset-svg name="my-icons" size="24">
+  <svg>
+    <defs>
+      <g id="arrow-back">
+        <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
+      </g>
+      <g id="menu">
+        <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+      </g>
+      <g id="chevron-right">
+        <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>
+      </g>
+      <g id="close">
+        <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+      </g>
+      <g id="account-circle">
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"></path>
+      </g>
+
+    </defs>
+  </svg>
+</iron-iconset-svg>`;
+
+document.head.appendChild($_documentContainer.content);
